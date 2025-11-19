5dfc751f95ecba15e1f500865b034fef2726d012
Luana Nobrega <luanadevops@gmail.com>
2025-11-19 10:34:55 -0300

    update airflow environmente

--- airflow/dags/__init__.py
+++ airflow/dags/__init__.py
@@ -1,10 +1,9 @@
-# __init__.py
 """
-Pacote de DAGs do Airflow.
+Pacote principal de DAGs do projeto Data Analytics (Apple + Fitbit).
 
-Este arquivo permite que a pasta `dags/` seja tratada como um módulo Python,
-o que facilita integração com Git, testes unitários e importações estruturadas.
+Permite importações estruturadas e organização modular.
 """
 
-# Opcional: definir o que será exportado
-__all__ = []
+from dags.configs import settings
+
+__all__ = ["settings"]
