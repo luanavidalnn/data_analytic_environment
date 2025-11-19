1851c37a9732d1f59026588ca36d37e12552da87
Luana Nobrega <luanadevops@gmail.com>
2025-11-19 10:32:06 -0300

    update airflow environmente

---
+++ airflow/dags/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+"""
+Pacote de DAGs do Airflow.
+
+Este arquivo permite que a pasta `dags/` seja tratada como um módulo Python,
+o que facilita integração com Git, testes unitários e importações estruturadas.
+"""
+
+# Opcional: definir o que será exportado
+__all__ = []
